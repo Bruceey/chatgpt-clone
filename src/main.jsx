6889a68a605e50696b8cfd2bd9140f6efd82fa8c
bruceey <1763423828@qq.com>
2025-07-11 15:00:13 +0800

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router";
+import Homepage from "./routes/homepage/Homepage";
+import DashboardPage from "./routes/dashboardPage/DashboardPage";
+import ChatPage from "./routes/chatPage/ChatPage";
+import RootLayout from "./layouts/RootLayout/rootLayout";
+import DashboardLayout from "./layouts/dashboardLayout/DashboardLayout";
+
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <Homepage /> },
+      {
+        Component: DashboardLayout,
+        children: [
+          { path: "/dashboard", element: <DashboardPage /> },
+          { path: "/dashboard/chats/:id", element: <ChatPage /> },
+        ],
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
